ef2c4d5fc1c09110677012782960eeacc2628f4d
BluishRed <bluishredstudios@gmail.com>
2022-09-12 09:55:47 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ASCII_Art_Generator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
